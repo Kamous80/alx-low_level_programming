d01e19d893811323ccc900a890c1ca23e5be5801
Abdulganiyu Kamaldeen <abdulganiyykamaldeen@gmail.com>
2022-09-08 20:05:17 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(main)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	printf(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
