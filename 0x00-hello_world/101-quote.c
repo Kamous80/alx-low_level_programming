40a57dd9035d42fd180abf1d49ae64e39c1bdff5
Abdulganiyu Kamaldeen <abdulganiyykamaldeen@gmail.com>
2022-09-08 20:32:43 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main(main)
-{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+int main(void)
+{ 
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
