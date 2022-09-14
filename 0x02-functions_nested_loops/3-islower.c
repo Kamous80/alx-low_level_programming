725751fd954895a5f7a8500d4c1d6838225c2fb0
Abdulganiyu Kamaldeen <abdulganiyykamaldeen@gmail.com>
2022-09-13 17:15:19 -0700

    lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
- *_islower - Checks if a character is lowercase.
+ * _islower - Checks if a character is lowercase.
  * @c: The character to be checked.
  * 
  * Return: 1 if character is lowercase, 0 otherwise.
@@ -18,6 +18,5 @@ if (c >= 'a' && c <= 'z')
 	return (1);
 }
 else
-
 	return (0);
 }
