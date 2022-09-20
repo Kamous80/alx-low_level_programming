1b3eb544bdb5213805e52f7b19e5457839e33d01
Abdulganiyu Kamaldeen <abdulganiyykamaldeen@gmail.com>
2022-09-19 17:56:43 -0700

    Half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,14 +8,13 @@
  */
 void puts_half(char *str)
 {
-	int j= 0;
+	int j = 0;
 	int k;
 
 	while (str[j] != '\0')
 	{
 		j++;
 	}
-	
 	if (j % 2 == 1)
 	{
 		k = (j - 1) / 2;
@@ -25,7 +24,6 @@ void puts_half(char *str)
 	{
 		k = j / 2;
 	}
-	
 	for (; k < j; k++)
 	{
 		putchar(str[k]);
