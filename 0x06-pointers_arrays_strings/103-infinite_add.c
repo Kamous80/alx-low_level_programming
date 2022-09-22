cc37b1006397c700c24f25bd3a4ade071ea160b8
Abdulganiyu Kamaldeen <abdulganiyykamaldeen@gmail.com>
2022-09-21 20:56:52 -0700

    Adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,7 +10,7 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+int i, j, k, l, m, n;
 
 	for (i = 0; n1[i]; i++)
 		;
@@ -18,7 +18,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		;
 	if (i > size_r || j > size_r)
 		return (0);
-		m = 0;
+	m = 0;
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
 		n = m;
@@ -36,11 +36,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	r[k] = '\0';
 	if (i >= 0 || j >= 0 || m)
 		return (0);
-		for (k -= 1, l = 0; l < k; k--, l++)
-		{
-			m = r[k];
-			r[k] = r[l];
-			r[l] = m;
-		}
-		return (r);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
