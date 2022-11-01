6607f55699c344729965f11c67883aae2261000e
Abdulganiyu Kamaldeen <abdulganiyykamaldeen@gmail.com>
2022-10-31 19:54:50 -0700

    program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -11,6 +11,7 @@ void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
 void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
 void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
@@ -177,7 +178,7 @@ void print_osabi(unsigned char *e_ident)
  * print_abi - Prints the ABI version of an ELF header.
  * @e_ident: A pointer to an array containing the ELF ABI version.
  */
-void print_abi(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident)
 {
 	printf("  ABI Version:                       %d\n",
 		e_ident[EI_ABIVERSION]);
