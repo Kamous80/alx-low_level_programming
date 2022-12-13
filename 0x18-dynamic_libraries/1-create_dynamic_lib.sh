c0325f91323be3afd61da9adad545086fa575e79
Abdulganiyu Kamaldeen <abdulganiyykamaldeen@gmail.com>
2022-12-12 18:14:56 -0800

    dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcc -Wall -Werror -Wextra -pedantic -c -fPIC *.c
 gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=:/alx-low_level_programming/0x18-dynamic_libraries
