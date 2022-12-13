a296b3cf0c66efdba51fc9f08c656fee9da9e340
Abdulganiyu Kamaldeen <abdulganiyykamaldeen@gmail.com>
2022-12-13 05:46:36 -0800

    arithmetic

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Kamous80/alx-low_level_programming/blob/master/0x18-dynamic_libraries/rand.so
-export LD_PRELOAD=/tmp/rand.so
+wget https://github.com/holbertonschool/0x18.c.git
